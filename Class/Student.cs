db2019786fda2bb33eab195fdd8df94acea45b49
Anony <41979552+dlwhdvlf71@users.noreply.github.com>
2022-06-15 19:02:18 +0900

    Commit

--- Class/Student.cs
+++ Class/Student.cs
@@ -27,6 +27,14 @@ namespace MauiApp1.Class
                 new Student() { Name = "홍길동1", Age = 29, Gender = "M" }
                 , new Student() { Name = "홍길동2", Age = 22, Gender = "W" }
                 , new Student() { Name = "홍길동3", Age = 18, Gender = "M" }
+                , new Student() { Name = "홍길동4", Age = 18, Gender = "M" }
+                , new Student() { Name = "홍길동5", Age = 18, Gender = "M" }
+                , new Student() { Name = "홍길동6", Age = 18, Gender = "W" }
+                , new Student() { Name = "홍길동7", Age = 18, Gender = "M" }
+                , new Student() { Name = "홍길동8", Age = 18, Gender = "M" }
+                , new Student() { Name = "홍길동9", Age = 18, Gender = "W" }
+                , new Student() { Name = "홍길동10", Age = 18, Gender = "W" }
+                , new Student() { Name = "홍길동11", Age = 18, Gender = "M" }
             };
         }
     }
