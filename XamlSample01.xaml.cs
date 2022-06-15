94028f8bce4add9a2740e0b6d45f70668070de46
Anony <41979552+dlwhdvlf71@users.noreply.github.com>
2022-06-15 17:36:46 +0900

    Commit

--- XamlSample01.xaml.cs
+++ XamlSample01.xaml.cs
@@ -1,5 +1,12 @@
 namespace MauiApp1;
 
+
+
+public static class TestClass
+{
+    public static string test = "test";
+}
+
 public partial class XamlSample01 : ContentPage
 {
     public XamlSample01()
