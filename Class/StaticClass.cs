7da7afa9b9f8df9fb3647c320fdd14f8e9f304c6
Anony <41979552+dlwhdvlf71@users.noreply.github.com>
2022-08-11 18:23:08 +0900

    Commit

---
+++ Class/StaticClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Class
+{
+    static class StaticClass
+    {
+        public static readonly Color TestColor = Colors.Aqua;
+
+    }
+}
