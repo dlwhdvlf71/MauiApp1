4757fba5e72b3594ff90d7af6b6136f31b85d912
Anony <41979552+dlwhdvlf71@users.noreply.github.com>
2022-10-26 18:10:53 +0900

    Commit

---
+++ SharedResource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    static class SharedResource
+    {
+        public static readonly Color FontColor = Color.FromRgb(0, 0, 0xff);
+        public static readonly Color BackgroundColor = Color.FromRgb(0xff, 0xff, 0xAD);
+
+    }
+}
--- XamlSample02.xaml.cs
+++ XamlSample02.xaml.cs
@@ -2,8 +2,31 @@ namespace MauiApp1;
 
 public partial class XamlSample02 : ContentPage
 {
-	public XamlSample02()
-	{
-		InitializeComponent();
-	}
+    public XamlSample02()
+    {
+        InitializeComponent();
+    }
+
+    private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+    {
+        Label label = (Label)sender;
+        Color[] colors = { Color.FromRgb(240, 248, 255), Color.FromRgb(255, 160, 122), Color.FromRgb(32, 178, 170), Color.FromRgb(255, 0, 255) };
+
+        Random random = new Random();
+
+        label.TextColor = colors[random.Next(colors.Count() - 1)];
+    }
+
+
+    private void OnDark_Clicked(object sender, EventArgs e)
+    {
+        Resources["fgColor"] = Colors.Navy;
+        Resources["bgColor"] = Colors.Silver;
+    }
+
+    private void OnLight_Clicked(object sender, EventArgs e)
+    {
+        Resources["fgColor"] = Colors.Silver;
+        Resources["bgColor"] = Colors.Navy;
+    }
 }
