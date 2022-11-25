b9ae5e7a3c8da604ab021e46ee9bab189cb33ed6
Anony <41979552+dlwhdvlf71@users.noreply.github.com>
2022-11-25 14:16:06 +0900

    Commit
    CommunityToolkit 추가

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,30 +10,30 @@ public partial class MainPage : ContentPage
         //InitBinding();
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
-    {
-        count++;
+    //private void OnCounterClicked(object sender, EventArgs e)
+    //{
+    //    count++;
 
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
+    //    if (count == 1)
+    //        CounterBtn.Text = $"Clicked {count} time";
+    //    else
+    //        CounterBtn.Text = $"Clicked {count} times";
 
-        SemanticScreenReader.Announce(CounterBtn.Text);
-    }
+    //    SemanticScreenReader.Announce(CounterBtn.Text);
+    //}
 
-    private async void OnTimerClicked(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new XamlSample02());
-    }
+    //private async void OnTimerClicked(object sender, EventArgs e)
+    //{
+    //    await Navigation.PushAsync(new XamlSample02());
+    //}
 
-    public void InitBinding()
-    {
-        dotnetBotImage.SetBinding(Image.RotationProperty, new Binding("Value", source:sliderImage));
+    //public void InitBinding()
+    //{
+    //    dotnetBotImage.SetBinding(Image.RotationProperty, new Binding("Value", source:sliderImage));
 
 
-        //dotnetBotImage.BindingContext = sliderImage;
-        //dotnetBotImage.SetBinding(Image.RotationProperty, "Value");   
-    }
+    //    //dotnetBotImage.BindingContext = sliderImage;
+    //    //dotnetBotImage.SetBinding(Image.RotationProperty, "Value");   
+    //}
 }
 
---
+++ SourceGeneratorViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MauiApp1
+{
+    public partial class SourceGeneratorViewModel : ObservableObject
+    {
+        public SourceGeneratorViewModel()
+        {
+            Name = "Name";
+        }
+
+        /*
+         * ObservableProperty 사용시 필드명 Pattern
+         * 1. lowerCamel
+         * 2. _lowerCamel
+         * 3. m_lowerCamel
+         * */
+        [ObservableProperty]
+        private string m_name;
+        
+
+        [RelayCommand]
+        private void ChangeName() => Name = "Change New Name";
+    }
+}
