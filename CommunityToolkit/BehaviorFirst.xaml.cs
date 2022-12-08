121d194f72dfcf6b52079b113188858133151220
Anony <41979552+dlwhdvlf71@users.noreply.github.com>
2022-12-08 16:55:22 +0900

    Commit
    CommunityToolkit Behavior-1

---
+++ CommunityToolkit/BehaviorFirst.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1.CommunityToolkit;
+
+public partial class BehaviorFirst : ContentPage
+{
+	public BehaviorFirst()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CommunityToolkit/BehavoirFirstViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.CommunityToolkit
+{
+    public partial class BehavoirFirstViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string message;
+
+        [RelayCommand]
+        private void Click() => Message = "눌렀다";
+    }
+}
