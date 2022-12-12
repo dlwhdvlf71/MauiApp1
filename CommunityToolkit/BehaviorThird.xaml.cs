5620a6b5301d4418fd2e01fe77f9e4862de57161
Anony <41979552+dlwhdvlf71@users.noreply.github.com>
2022-12-12 16:36:16 +0900

    Commit
    1. SetFocusOnEntryCompleteBehavior
    2. UriValidationBehavior
    3. UserStoppedTypingBehavior

---
+++ CommunityToolkit/BehaviorThird.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1.CommunityToolkit;
+
+public partial class BehaviorThird : ContentPage
+{
+	public BehaviorThird()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CommunityToolkit/BehaviorThirdViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.CommunityToolkit
+{
+    public partial class BehaviorThirdViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string message;
+
+        [RelayCommand]
+        private void Search()
+        {
+            Message = $"{DateTime.Now} Search 결과입니다.";
+        }
+    }
+}
