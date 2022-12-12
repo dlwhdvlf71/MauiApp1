ace9c5ded6ab6ed9085388a60c1974fefeaa8786
Anony <41979552+dlwhdvlf71@users.noreply.github.com>
2022-12-12 14:32:54 +0900

    Commit
    1. ProgressBarAnimationBehavior
    2. RequiredStringValidationBehavior

--- CommunityToolkit/BehaviorSecondViewModel.cs
+++ CommunityToolkit/BehaviorSecondViewModel.cs
@@ -10,14 +10,37 @@ namespace MauiApp1.CommunityToolkit
 {
     public partial class BehaviorSecondViewModel : ObservableObject
     {
-        [ObservableProperty]
-        public string message;
+        public BehaviorSecondViewModel()
+        {
+            Progress = 0.0d;
+        }
 
+        [ObservableProperty]
+        private string message;
 
         [RelayCommand]
         private void MaxLengthReached()
         {
             Message = "최대 길이에 도달했다";
         }
+
+        [ObservableProperty]
+        private double progress;
+
+
+        [RelayCommand]
+        private async void Execute()
+        {
+            Progress = 0.0d;
+            await Task.Delay(500);
+
+            for(int i = 0; i <= 10; i++)
+            {
+                Progress += 0.1;
+                await Task.Delay(500);
+            }
+
+        }
+
     }
 }
